2780fdf5f497e6b5688c60f2a64c4e6be145f48d
mikeyxx <michaelogboo@gmail.com>
2023-03-05 09:04:09 +0100

    Trying to resolve path name

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ app.use("/api/v1/", userAuth, questionsRouter);
 
 app.use(express.static(path.join(__dirname, "./client/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
+  var filePath = "./client/dist/index.html";
+  var resolvedPath = path.resolve(filePath);
+  console.log(resolvedPath);
+  return res.sendFile(resolvedPath);
 });
 
 app.use(errorHandlerMiddleware);
